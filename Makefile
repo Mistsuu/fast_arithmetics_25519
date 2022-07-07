4dcb2bf77b2bb6215f45fb38efcffae8ec1e74a6
Mistsu <vietanhtran12@gmail.com>
2022-07-07 14:35:05 +0700

    dbg

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ SOURCE_DIR  = src
 
 # Compilers options
 CC          = gcc
-CFLAGS      = -I$(LIBRARY_DIR) -O3 -ggdb
+CFLAGS      = -I$(LIBRARY_DIR) -O0 -ggdb
 LIBS		= 
 
 # Requirements and stuffs
--- src/main.c
+++ src/main.c
@@ -12,20 +12,10 @@ int main(int argc, char **argv)
         {9, 0, 0, 0, 0},
         {1, 0, 0, 0, 0}
     };
-    uDBL(&P, &P);
+    PointMo25519 R;
 
+    uDBL(&P, &R);
 
-    printf("0x%lx\n", P.u.limbs[0]);
-    printf("0x%lx\n", P.u.limbs[1]);
-    printf("0x%lx\n", P.u.limbs[2]);
-    printf("0x%lx\n", P.u.limbs[3]);
-    printf("0x%lx\n", P.u.limbs[4]);
-
-    printf("0x%lx\n", P.w.limbs[0]);
-    printf("0x%lx\n", P.w.limbs[1]);
-    printf("0x%lx\n", P.w.limbs[2]);
-    printf("0x%lx\n", P.w.limbs[3]);
-    printf("0x%lx\n", P.w.limbs[4]);
 
     return 0;
 }
