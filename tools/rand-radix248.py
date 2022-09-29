4917f21f9dd89e60a99fd2dfe2ef1f45cff2196c
Mistsu <vietanhtran12@gmail.com>
2022-09-29 15:55:59 +0700

    Update tools

--- tools/rand-radix248.py
+++ tools/rand-radix248.py
@@ -2,4 +2,4 @@ from random  import randrange
 from arr2num import getfromradix248
 from num2arr import splitradix248
 
-getfromradix248(splitradix248(randrange(0, 2**142-111)))
+getfromradix248(splitradix248(randrange(0, 2**142-111), debug=True), debug=True)
